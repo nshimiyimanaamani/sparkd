ad65141ba8656af4145bd1fd8ef52d421036a31e
iradukunda1 <iradujdd4@gmail.com>
2023-06-29 16:11:39 +0200

    disable signal

--- internal/services/vmms/start.go
+++ internal/services/vmms/start.go
@@ -21,9 +21,9 @@ func (*Options) Start(m *core.Firecracker) (*core.Firecracker, error) {
 	}
 	// defer m.Vm.StopVMM()
 
-	if v := InstallSignalHandlers(ctx, m); <-v {
-		fmt.Println("signal received")
-	}
+	// if v := InstallSignalHandlers(ctx, m); <-v {
+	// 	fmt.Println("signal received")
+	// }
 
 	go func() {
 		defer m.CancelCtx()
